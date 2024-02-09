b7f5cdd086f120fab00eb80175493a89f2baf62a
Axel <44190680+MLRAleader@users.noreply.github.com>
2024-02-09 02:53:12 +0100

    Update index.js
    We added keydown listener functionality on each sound so we can play with the keyboard key.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,23 @@
+console.time("Temps");
+
+//Click
 var drums = document.querySelectorAll(".drum");
 var i = 0;
 do {
   drums[i].addEventListener("click", function () {
     var courentObject = this.innerHTML;
-    switch (courentObject) {
+    goodKeyPlaySound(courentObject);
+  });
+  i++;
+} while (i < drums.length);
+
+
+//Keydown
+document.addEventListener("keydown", function (Event) {
+  goodKeyPlaySound(Event.key);
+});
+function goodKeyPlaySound(evenementDeclencheur) { 
+    switch (evenementDeclencheur) {
       case "w":
         let audioW = new Audio("assets/sounds/tom-1.mp3");
         audioW.play();
@@ -13,29 +27,32 @@ do {
         audioA.play();
         break;
       case "s":
-          let audioS = new Audio("assets/sounds/tom-3.mp3");
-          audioS.play();
+        let audioS = new Audio("assets/sounds/tom-3.mp3");
+        audioS.play();
         break;
-        case "d":
-          let audioD = new Audio("assets/sounds/tom-4.mp3");
+      case "d":
+        let audioD = new Audio("assets/sounds/tom-4.mp3");
           audioD.play();
-        break;
-        case "j":
-          let audioJ = new Audio("assets/sounds/snare.mp3");
+          break;
+      case "j":
+        let audioJ = new Audio("assets/sounds/snare.mp3");
           audioJ.play();
-        break;
-        case "k":
-          let audioK = new Audio("assets/sounds/crash.mp3");
+          break;
+      case "k":
+        let audioK = new Audio("assets/sounds/crash.mp3");
           audioK.play();
-        break;
-        case "l":
-          let audioL = new Audio("assets/sounds/kick-bass.mp3");
+          break;
+      case "l":
+        let audioL = new Audio("assets/sounds/kick-bass.mp3");
           audioL.play();
           break;
+      case "d":
       default:
-        aler("You must click on a letter!");
+        alert("You have clicked on a wrong key");
         break;
     }
-  })
-  i++;
-}while(i<drums.length)
+  }
+
+ 
+
+console.timeEnd("Temps");
